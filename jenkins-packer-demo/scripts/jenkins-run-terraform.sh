f82b76f67fcca165179f7b34d5783022e9f178db
RosaHdz <63610273+RosaHdz@users.noreply.github.com>
2020-05-15 19:36:34 -0500

    Update jenkins-run-terraform.sh

--- jenkins-packer-demo/scripts/jenkins-run-terraform.sh
+++ jenkins-packer-demo/scripts/jenkins-run-terraform.sh
@@ -4,7 +4,6 @@ AWS_REGION="us-east-1"
 cd jenkins-packer-demo
 pwd
 ls
-rm ./terraform
 S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
 sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
